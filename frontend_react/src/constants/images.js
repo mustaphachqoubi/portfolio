f48115a8da0fa78a7be5df18e25c82219b8d9a3f
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-03-02 10:49:51 +0100

    fixed sass compiling problem

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,22 +1,22 @@
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
+// import api from '../assets/api.png';
+// import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
+// import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
-import graphql from '../assets/graphql.png';
+// import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import mu5 from '../assets/mu5.png';
 import node from '../assets/node.png';
-import python from '../assets/python.png';
+// import python from '../assets/python.png';
 import react from '../assets/react.png';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
+// import vue from '../assets/vue.png';
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -29,7 +29,7 @@ import logo from '../assets/logo.png';
 
 import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
+// import asus from '../assets/asus.png';
 import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
@@ -38,23 +38,23 @@ import spotify from '../assets/spotify.png';
 export default {
   email,
   mobile,
-  api,
-  cpp,
+  // api,
+  // cpp,
   css,
   figma,
-  flutter,
+  // flutter,
   git,
-  graphql,
+  // graphql,
   html,
   javascript,
   mu5,
   node,
-  python,
+  // python,
   react,
   redux,
   sass,
   typescript,
-  vue,
+  // vue,
   about01,
   about02,
   about03,
@@ -64,7 +64,7 @@ export default {
   logo,
   adidas,
   amazon,
-  asus,
+  // asus,
   bolt,
   nb,
   skype,
